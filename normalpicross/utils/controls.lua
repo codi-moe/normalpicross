ade40f556ca09043bacd3a1c7f22366cd61555c1
Codinget <codinget@codi.moe>
2026-03-06 19:59:01 +0000

    feat: use controls from save data

--- normalpicross/utils/controls.lua
+++ normalpicross/utils/controls.lua
@@ -1,33 +1,101 @@
+local Save = require "normalpicross.utils.save"
+
 local controls = {}
 
+local config = Save:new("controls.lua", {
+    primary = {
+        { key = 'space' },
+        { key = 'return' },
+        { scancode = 'e' },
+        { scancode = 'z' },
+    },
+    secondary = {
+        { key = 'backspace' },
+        { key = 'lshift' },
+        { key = 'rshift' },
+        { scancode = 'q' },
+        { scancode = 'x' },
+    },
+    menu = {
+        { key = 'escape' },
+        { scancode = 'c' },
+    },
+    up = {
+        { key = 'up' },
+        { scancode = 'w' },
+    },
+    down = {
+        { key = 'down' },
+        { scancode = 's' },
+    },
+    left = {
+        { key = 'left' },
+        { scancode = 'a' },
+    },
+    right = {
+        { key = 'right' },
+        { scancode = 'd' },
+    },
+})
+function config:check()
+    for k, v in pairs(self.default) do
+        if not self.data[k] then
+            self.data[k] = v
+        end
+    end
+    for k in pairs(self.data) do
+        if not self.default[k] then
+            self.data[k] = nil
+        end
+    end
+    return true
+end
+
+local function update()
+    config:load()
+    config:save()
+
+    for k, v in pairs(config.data) do
+        controls[k] = { key = {}, scancode = {} }
+        for _, obj in ipairs(v) do
+            local t, r = next(obj)
+            ---@diagnostic disable-next-line: need-check-nil
+            controls[k][t][r] = true
+        end
+    end
+end
+update()
+
+function controls.is(type, key, scancode)
+    return controls[type].key[key] or controls[type].scancode[scancode] or false
+end
+
 function controls.isprimary(key, scancode)
-    return key == 'space' or key == 'return'
-        or scancode == 'e' or scancode == 'z'
+    return controls.is('primary', key, scancode)
 end
 
 function controls.issecondary(key, scancode)
-    return key == 'backspace' or key == 'lshift' or key == 'rshift'
-        or scancode == 'q' or scancode == 'x'
+    return controls.is('secondary', key, scancode)
 end
 
 function controls.ismenu(key, scancode)
-    return key == 'escape' or scancode == 'c'
+    return controls.is('menu', key, scancode)
 end
 
 function controls.isup(key, scancode)
-    return key == 'up' or scancode == 'w'
+    return controls.is('up', key, scancode)
 end
 
 function controls.isdown(key, scancode)
-    return key == 'down' or scancode == 's'
+    return controls.is('down', key, scancode)
 end
 
 function controls.isleft(key, scancode)
-    return key == 'left' or scancode == 'a'
+    return controls.is('left', key, scancode)
 end
 
 function controls.isright(key, scancode)
-    return key == 'right' or scancode == 'd'
+    return controls.is('right', key, scancode)
 end
 
 return controls
